c6ec76beccd344d8ea5d5415244d9c6ad13f915f
reagancheatham <reagancheatham@gmail.com>
2026-01-31 16:46:24 -0600

    Add punch state

---
+++ Assets/Scripts/CultMask/Players/States/PlayerGroundedPunchState.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace CultMask.Players
+{
+    public class PlayerGroundedPunchState : PlayerState
+    {
+        protected override void OnEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnExit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
